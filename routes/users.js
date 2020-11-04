91d83120db92512e9410a3188e3cbde89d56947a
kkasper <kkasper>
2020-11-04 09:18:27 -0700

    Added page structure & inheritance, added home page and login pages. Added graphics.

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('login.njk', { title: 'Express' });
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login.njk', { title: 'Express' });
 });
 
 module.exports = router;
