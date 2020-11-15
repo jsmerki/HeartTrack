ca471022f05804962cc4c756cd7dc0d6c97d3405
kkasper <kkasper>
2020-11-15 15:37:38 -0700

    Finished registration.

--- public/javascripts/register.js
+++ public/javascripts/register.js
@@ -4,43 +4,93 @@ function sendRegisterRequest() {
     let password = $('#inputPassword');
     let passwordConfirm = $('#confirmPassword');
     let fullName = $('#inputFullName');
-    console.log('Callback run.');
-    $(".invalid-feedback").hide();
 
-    console.log(password.val());
+    // Reset form on each submit
+    $(".invalid-feedback").hide();
+    $(".is-invalid").removeClass("is-invalid").removeClass('is-valid');
+    $(".requiredField").remove();
+    $("#passwordCriterion").hide();
+    let error = false;
+
+
     /* *************** *
      *  Error Checking *
      * *************** */
-    if (email.val() !== emailConfirm.val()) {
+    // Valid Email checking
+    let email_re = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}/;
+
+    if (! (email_re.test(email.val())) ) {
+        email.addClass("is-invalid");
+        $('.invalidEmail').show();
+        error = true;
+        console.log('Email is invalid.');
+    }
+
+    // Email matching confirm checking
+    if (email.val() != emailConfirm.val()) {
         email.addClass("is-invalid");
         emailConfirm.addClass("is-invalid");
         $('.unmatchedEmails').show();
-        console.log('Emails invalid.');
-        return;
+        error = true;
+        console.log('Emails do not match.');
+    }
+
+    // Password matching confirm checking
+    if (password.val() != passwordConfirm.val()) {
+        password.addClass("is-invalid");
+        passwordConfirm.addClass("is-invalid");
+        $('.unmatchedPasswords').show();
+        error = true;
+        console.log('Passwords do not match.');
+    }
+
+    /* Password strength regex uses look-aheads to confirm
+        1. At least one uppercase character
+        2. At least one special character
+        3. At least one digit
+        4. At least one lower case
+        5. At least 8 characters
+    */
+    let passStrength_re = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*]).{8,}/;
+    // Password matching confirm checking
+    if ( !(passStrength_re.test(password.val()))) {
+        password.addClass("is-invalid");
+        passwordConfirm.addClass("is-invalid");
+        $('.weakPassword').show();
+        error = true;
+        $("#passwordCriterion").show();
+    }
+
+    $('input').each( function(index) {
+        let input = $(this);
+
+        if(input.val().length < 1){
+            input.addClass("is-invalid");
+            input.after($( '<div class="invalid-feedback requiredField">This field is required!</div>' ));
+            error = true;
+        }
+    })
+    // This is required for some reason??
+    $(".requiredField").show();
+
+    $("input:not(.invalid-feedback)").addClass('is-valid');
+
+    if(!error) {
+        console.log("--");
+        /*
+        $.ajax({
+            url: '/user/register',
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify({email: email.val(), fullName: fullName.val(), password: password.val()}),
+            dataType: 'json'
+        });
+        */
+
     }
     else {
-        email.removeClass("is-invalid").addClass("is-valid");
-        emailConfirm.removeClass("is-invalid").addClass("is-valid");
-        $('.unmatchedEmails').hide();
-        console.log('Emails valid.')
+        console.log("--");
     }
-
-    // Check to make sure the passwords match
-    // FIXME: Check to ensure strong password
-    if (password.val() != passwordConfirm.val()) {
-        $('#ServerResponse').html("<span class='red-text text-darken-2'>Passwords do not match.</span>");
-        $('#ServerResponse').show();
-        return;
-    }
-
-
-    $.ajax({
-        url: '/user/register',
-        type: 'POST',
-        contentType: 'application/json',
-        data: JSON.stringify({email:email.val(), fullName:fullName.val(), password:password.val()}),
-        dataType: 'json'
-    });
         //.done(registerSuccess)
         //.fail(registerError);
 
