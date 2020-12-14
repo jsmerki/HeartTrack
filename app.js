3d77b05b30b5be81c1d096dd416851331791c648
EC2 Default User <ec2-user@ip-172-31-30-95.us-east-2.compute.internal>
2020-12-14 23:24:35 +0000

    finished https

--- app.js
+++ app.js
@@ -56,6 +56,14 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-
+app.use (function (req, res, next) {
+        if (req.secure) {
+                // request was via https, so do no special handling
+                next();
+        } else {
+                // request was via http, so redirect to https
+                res.redirect('https://' + req.headers.host + req.url);
+        }
+});
 
 module.exports = app;
